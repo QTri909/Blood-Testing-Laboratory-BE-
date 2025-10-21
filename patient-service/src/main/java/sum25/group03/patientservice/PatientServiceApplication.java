f73d9bba673c549a91a9ae14ad6b9f5c1e77e9e7
Ha Hai Cuong <hhc9104@gmail.com>
2025-10-21 22:58:17 +0700

    patient-service:adjust(Main file) - update content

--- patient-service/src/main/java/sum25/group03/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/sum25/group03/patientservice/PatientServiceApplication.java
@@ -3,10 +3,7 @@ package sum25.group03.patientservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {
-        "sum25.group03.patientservice",
-        "sum25.group03.patientservice.exception.global"
-})
+@SpringBootApplication
 public class PatientServiceApplication {
 
 	public static void main(String[] args) {
