43f9c3b18e6f53b51443a2dbf4d86b1084b13be5
Ha Hai Cuong <hhc9104@gmail.com>
2025-10-24 09:05:29 +0700

    patient-service:add(feature) - admin watch logs from audit logs from mongodb AuditEntry

--- patient-service/src/main/java/sum25/group03/patientservice/enums/ActionTypeFeatures.java
+++ patient-service/src/main/java/sum25/group03/patientservice/enums/ActionTypeFeatures.java
@@ -1,6 +1,10 @@
 package sum25.group03.patientservice.enums;
 
 public enum ActionTypeFeatures {
+    // patient:
     VIEW_PATIENT_MEDICAL_RECORD_DETAIL,
-    VIEW_ALL_PATIENT_MEDICAL_RECORDS
+    VIEW_ALL_PATIENT_MEDICAL_RECORDS,
+
+    // audit:
+    VIEW_AUDIT_LOGS
 }
--- patient-service/src/main/java/sum25/group03/patientservice/services/interfaces/AuditEntryMongoService.java
+++ patient-service/src/main/java/sum25/group03/patientservice/services/interfaces/AuditEntryMongoService.java
@@ -1,7 +1,12 @@
 package sum25.group03.patientservice.services.interfaces;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import sum25.group03.patientservice.documents.AuditEntryDocument;
+import sum25.group03.patientservice.dtos.response.AuditEntryResponseDTO;
+import sum25.group03.patientservice.enums.DocumentType;
 
 public interface AuditEntryMongoService {
     void saveAuditEntry(AuditEntryDocument auditEntryDocument);
+    Page<AuditEntryResponseDTO> queryLogsWithPagination(Pageable pageable, DocumentType documentType, Long viewerId);
 }
