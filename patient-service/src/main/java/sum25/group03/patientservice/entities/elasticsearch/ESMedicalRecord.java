dfb2916da15153ff8e6fe5a9f022075513712848
Ha Hai Cuong <hhc9104@gmail.com>
2025-10-30 00:59:30 +0700

    patient-service:add(ES entity) - define ES entity for clinical notes and user snapshot

--- patient-service/src/main/java/sum25/group03/patientservice/entities/elasticsearch/ESMedicalRecord.java
+++ patient-service/src/main/java/sum25/group03/patientservice/entities/elasticsearch/ESMedicalRecord.java
@@ -3,18 +3,20 @@ package sum25.group03.patientservice.entities.elasticsearch;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 import sum25.group03.patientservice.enums.MedicalRecordStatus;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Document(indexName = "medical_records")
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 @Builder
-public class MedicalRecord {
+public class ESMedicalRecord {
     @Id
     private Long recordId;
     private UUID recordCode;
@@ -24,4 +26,7 @@ public class MedicalRecord {
     private LocalDateTime visitDate;
     private Long createdBy;
     private Long updatedBy;
+
+    @Field(type = FieldType.Nested)
+    private List<ESClinicalNote> clinicalNotes;
 }
--- patient-service/src/main/java/sum25/group03/patientservice/repositories/elasticsearch/MedicalRecordESRepository.java
+++ patient-service/src/main/java/sum25/group03/patientservice/repositories/elasticsearch/MedicalRecordESRepository.java
@@ -2,10 +2,10 @@ package sum25.group03.patientservice.repositories.elasticsearch;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
-import sum25.group03.patientservice.entities.elasticsearch.MedicalRecord;
+import sum25.group03.patientservice.entities.elasticsearch.ESMedicalRecord;
 
 @Repository
-public interface MedicalRecordESRepository extends ElasticsearchRepository<MedicalRecord, Long> {
-    MedicalRecord findByPatientId(Long patientId);
-    MedicalRecord findByAssignedUser(Long assignedUser);
+public interface MedicalRecordESRepository extends ElasticsearchRepository<ESMedicalRecord, Long> {
+    ESMedicalRecord findByPatientId(Long patientId);
+    ESMedicalRecord findByAssignedUser(Long assignedUser);
 }
