4ffad640e13c1f5be1803821d4b36d26578778a7
Ha Hai Cuong <hhc9104@gmail.com>
2025-10-31 17:01:21 +0700

    patient-service:remove(redundant-import) - remove redundant import of grpc testorder response

--- patient-service/src/main/java/sum25/group03/patientservice/services/interfaces/PatientService.java
+++ patient-service/src/main/java/sum25/group03/patientservice/services/interfaces/PatientService.java
@@ -1,8 +1,6 @@
 package sum25.group03.patientservice.services.interfaces;
 
-import sum25.group03.patientservice.dtos.response.MedicalRecordResponse;
 import sum25.group03.patientservice.dtos.response.PatientResponseDTO;
-import sum25.group03.patientservice.grpc.TestOrderResponse;
 import sum25.group03.patientservice.grpc.dtos.GrpcTestOrderDTO;
 
 import java.util.List;
