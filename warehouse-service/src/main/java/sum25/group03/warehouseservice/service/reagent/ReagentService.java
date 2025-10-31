0d6488356274937d2edecc7b62c375677e337fe0
Tri <quachtri865@gmail.com>
2025-10-31 11:34:31 +0700

    feat(reagent): Add methods for retrieving and deleting reagents; implement Kafka event publishing for deletions

--- warehouse-service/src/main/java/sum25/group03/warehouseservice/service/reagent/ReagentService.java
+++ warehouse-service/src/main/java/sum25/group03/warehouseservice/service/reagent/ReagentService.java
@@ -2,12 +2,15 @@ package sum25.group03.warehouseservice.service.reagent;
 
 import sum25.group03.warehouseservice.dto.response.ReagentResponseForInstrument;
 import sum25.group03.warehouseservice.dto.response.ReagentValidationResponse;
+import sum25.group03.warehouseservice.entity.Reagents;
 
 import java.util.List;
 
 public interface ReagentService {
     List<Long> findExistingIds(List<Long> reagentIds);
-
+    List<Reagents> findAllByInstrumentId(Long instrumentId);
+    List<Reagents> findAllByReagentId(List<Long> reagentId);
+    void deleteReagent(Long reagentId);
     ReagentValidationResponse validateReagent(String batchNumber, Double requiredVolume);
 
     List<ReagentResponseForInstrument> listReagentsForInstrument();
