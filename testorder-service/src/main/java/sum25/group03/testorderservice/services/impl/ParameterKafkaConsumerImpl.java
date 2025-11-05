855b8c483eba5579241b1499911cf9189212a8bd
Ha Hai Cuong <hhc9104@gmail.com>
2025-11-05 15:29:19 +0700

    testorder-service:feat() - listener sync-parameters from warehouse's kafka pushing

--- testorder-service/src/main/java/sum25/group03/testorderservice/services/impl/ParameterKafkaConsumerImpl.java
+++ testorder-service/src/main/java/sum25/group03/testorderservice/services/impl/ParameterKafkaConsumerImpl.java
@@ -1,21 +1,40 @@
 package sum25.group03.testorderservice.services.impl;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.common.internals.Topic;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sum25.group03.testorderservice.constants.KafkaConsumerVars;
-import sum25.group03.testorderservice.dtos.request.ParameterRequestDTO;
+import sum25.group03.testorderservice.dtos.request.KafkaParameterRequestDTO;
+import sum25.group03.testorderservice.entities.Parameter;
+import sum25.group03.testorderservice.mapper.ParameterMapper;
+import sum25.group03.testorderservice.repositories.ParameterRepository;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ParameterKafkaConsumerImpl {
 
-    @KafkaListener(topics = KafkaConsumerVars.PARAMETER_TOPIC, groupId = "${spring.kafka.consumer.group-id}")
-    public void listenParameterTopic(Object message) {
-        if (message instanceof ParameterRequestDTO) {
+    private final ParameterRepository parameterRepository;
+    private final ParameterMapper parameterMapper;
 
+
+    @KafkaListener(topics = KafkaConsumerVars.PARAMETER_TOPIC, groupId = "${spring.kafka.consumer.group-id}")
+    @Transactional
+    public void listenParameterTopic(KafkaParameterRequestDTO dtoParameter) {
+
+        Parameter searchedByCode = parameterRepository.findByParamCode(dtoParameter.getParamCode());
+        Parameter newEntity = parameterMapper.fromKafkaDto(dtoParameter);
+
+        if (searchedByCode == null) {
+            parameterRepository.save(newEntity);
+            log.info("Saved new parameter {}",newEntity);
+            return;
         }
+
+        // else: update existing entity
+        parameterMapper.updateFromKafkaDto(dtoParameter, newEntity);
+        log.info("Updated existing parameter {}",newEntity);
     }
 }
