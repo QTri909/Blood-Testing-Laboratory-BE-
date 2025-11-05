84eeb9c2c63e10995edefe532ba58d0b6610233f
HNTKiet <kiethntse182592@fpt.edu.vn>
2025-11-05 10:16:41 +0700

    monitoring-service( api response)

--- monitoring-service/src/main/java/sum25/group03/monitoringservice/service/EventLogService.java
+++ monitoring-service/src/main/java/sum25/group03/monitoringservice/service/EventLogService.java
@@ -1,6 +1,9 @@
 package sum25.group03.monitoringservice.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import sum25.group03.monitoringservice.kafka.MockProducer;
 import sum25.group03.monitoringservice.model.EventLog;
 import sum25.group03.monitoringservice.repository.EventLogRepository;
 
@@ -11,15 +14,21 @@ import java.util.Optional;
 @Service
 public class EventLogService {
     private final EventLogRepository eventLogRepo;
-    public EventLogService(EventLogRepository eventLogRepository) {
+    private final MockProducer mockProducer;
+    public EventLogService(EventLogRepository eventLogRepository,MockProducer mockProducer) {
         this.eventLogRepo = eventLogRepository;
+        this.mockProducer = mockProducer;
+    }
+    // send mock
+    public void sendMessage(){
+        mockProducer.sendMockEvent();
     }
     public EventLog addEventLog(EventLog eventLog){
         eventLog.setCreatedAt(Instant.now());
         return eventLogRepo.save(eventLog);
     }
-    public List<EventLog> getAllEventLogs(){
-        return eventLogRepo.findAllByOrderByCreatedAtDesc();
+    public Page<EventLog> getAllEventLogs(int page, int size){
+        return eventLogRepo.findAllByOrderByCreatedAtDesc(PageRequest.of(page, size));
     }
     public Optional<EventLog> getEventLog(String id) {
         return eventLogRepo.findById(id);
@@ -27,4 +36,5 @@ public class EventLogService {
     public List<EventLog> searchEventLogs(String action, String message, String operator) {
         return eventLogRepo.searchEventLogs(action, message, operator);
     }
+
 }
