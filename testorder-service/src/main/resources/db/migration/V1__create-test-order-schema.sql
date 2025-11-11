3215b0652cdd945685e3cd4cd8f2393d80094450
Ha Hai Cuong <hhc9104@gmail.com>
2025-11-11 08:08:18 +0700

    feat: - testorder: adjust database add test order status 'unassigned'

--- testorder-service/src/main/resources/db/migration/V1__create-test-order-schema.sql
+++ testorder-service/src/main/resources/db/migration/V1__create-test-order-schema.sql
@@ -84,7 +84,7 @@ CREATE TABLE test_order (
                             run_by BIGINT,
                             instrument_id BIGINT,
                             status VARCHAR(255) CHECK (status IN (
-                                                                  'PENDING','ONGOING','WAITING','CANCELED','COMPLETED','REVIEWED','AI_REVIEWED', 'WAITING_PAYMENT'
+                                                                  'UNASSIGNED','PENDING','ONGOING','WAITING','CANCELED','COMPLETED','REVIEWED','AI_REVIEWED', 'WAITING_PAYMENT'
                                 ))
 );
 
