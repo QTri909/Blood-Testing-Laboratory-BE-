b5b64dbb0205e5cfe4f82753c050c7697d8ca038
Ha Hai Cuong <hhc9104@gmail.com>
2025-11-11 18:36:50 +0700

    feat:iam-service - adjust iam-service routes

--- iam-service/src/main/java/sum25/group03/iamservice/controller/AuthController.java
+++ iam-service/src/main/java/sum25/group03/iamservice/controller/AuthController.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
