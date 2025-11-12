7949d5e746f493f15b1c2a4d94d36ce99d769061
Ha Hai Cuong <hhc9104@gmail.com>
2025-11-12 12:55:55 +0700

    feat: adjust testorder Main file

--- testorder-service/src/main/java/sum25/group03/testorderservice/TestorderServiceApplication.java
+++ testorder-service/src/main/java/sum25/group03/testorderservice/TestorderServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
-        "sum25.group03.testorder-service",
+        "sum25.group03.testorderservice",
         "sum25.group03.common"
 })
 public class TestorderServiceApplication {
