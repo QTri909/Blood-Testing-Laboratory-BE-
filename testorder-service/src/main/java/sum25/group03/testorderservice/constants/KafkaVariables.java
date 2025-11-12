d52eb2d416266a12d89fe79a70b32ac6f1b49512
Ha Hai Cuong <hhc9104@gmail.com>
2025-11-12 09:07:30 +0700

    feat:testorder - adjust kafka topic for creating new user

--- testorder-service/src/main/java/sum25/group03/testorderservice/constants/KafkaVariables.java
+++ testorder-service/src/main/java/sum25/group03/testorderservice/constants/KafkaVariables.java
@@ -6,7 +6,7 @@ public class KafkaVariables {
     public static final String RETENTION_MS = "retention.ms";
 
     // for test order:
-    public static final String TEST_ORDER_TOPIC = "test-order-topic";
+    public static final String TEST_ORDER_TOPIC = "test-order-user-created";
 
     // for parameters:
     public static final String PARAMETER_TOPIC = "parameter-topic";
