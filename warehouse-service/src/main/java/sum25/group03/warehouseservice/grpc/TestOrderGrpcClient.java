dd791422c5736914be0e9977cabcc0b299d291f0
Tri <quachtri865@gmail.com>
2025-11-19 18:20:37 +0700

    feat: implement TestOrder gRPC client for syncing parameters with Warehouse service

---
+++ warehouse-service/src/main/java/sum25/group03/warehouseservice/grpc/TestOrderGrpcClient.java
@@ -0,0 +1,42 @@
+package sum25.group03.warehouseservice.grpc;
+
+import lombok.RequiredArgsConstructor;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Service;
+import sum25.group03.common.response.dtos.grpc.ParameterGrpc;
+import sum25.group03.warehouse.grpc.SyncParameterRequest;
+import sum25.group03.warehouse.grpc.SyncParameterRequestList;
+import sum25.group03.warehouse.grpc.SyncParameterResponse;
+import sum25.group03.warehouse.grpc.TestOrderServiceGrpc;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+
+@Service
+@RequiredArgsConstructor
+public class TestOrderGrpcClient {
+    @GrpcClient("test-order-service")
+    private TestOrderServiceGrpc.TestOrderServiceBlockingStub stub;
+
+    public SyncParameterResponse syncParameter(List<ParameterGrpc> parameter){
+        List<SyncParameterRequest> requestList = parameter.stream()
+                .map(p -> SyncParameterRequest.newBuilder()
+                        .setId(p.getId())
+                        .setAbbreviation(p.getAbbreviation())
+                        .setParameterName(p.getParameterName())
+                        .setPrice(p.getPrice())
+                        .setDescription(p.getDescription())
+                        .setMinValue(p.getMinValue())
+                        .setMaxValue(p.getMaxValue())
+                        .setUnit(p.getUnit())
+                        .setGender(p.getGender())
+                        .build())
+                .toList();
+        SyncParameterRequestList request = SyncParameterRequestList.newBuilder()
+                .addAllParameters(requestList)
+                .build();
+        return stub.syncParameter(request);
+    }
+}
