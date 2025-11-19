111d6fd618ac2a07462bba6219ca9e8bc009808e
Tri <quachtri865@gmail.com>
2025-11-19 16:55:45 +0700

    fix error parameterMapper

---
+++ common/src/main/java/sum25/group03/common/response/dtos/grpc/ParameterGrpc.java
@@ -0,0 +1,25 @@
+package sum25.group03.common.response.dtos.grpc;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class ParameterGrpc {
+    private Long id;
+    private String abbreviation;
+    private String parameterName;
+    private Double price;
+    private String description;
+    private Double minValue;
+    private Double maxValue;
+    private String unit;
+    private String gender;
+
+}
