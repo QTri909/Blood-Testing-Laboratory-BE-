41e03d5d0ce8e1e41a636c1cc612de3571827206
cuongDora <hhc9104@gmail.com>
2025-11-21 14:05:37 +0700

    feat: add kafka to payment service

--- payment-service/src/main/java/sum25/group03/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/sum25/group03/payment_service/PaymentServiceApplication.java
@@ -3,12 +3,14 @@ package sum25.group03.payment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication(scanBasePackages = {
        "sum25.group03.payment_service",
        "sum25.group03.common"
 })
 @ConfigurationPropertiesScan
+@EnableKafka
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
