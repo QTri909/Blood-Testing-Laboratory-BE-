6c975eea63c3ced2f7e7ed3c2f6f9aad6ddd0a84
Tri <quachtri865@gmail.com>
2025-11-24 00:38:29 +0700

    fix: initialize quantityUsed to default value of 0.0 in ReagentHistoryUsage

--- warehouse-service/src/main/java/sum25/group03/warehouseservice/service/instrument/InstrumentServiceImpl.java
+++ warehouse-service/src/main/java/sum25/group03/warehouseservice/service/instrument/InstrumentServiceImpl.java
@@ -63,6 +63,7 @@ public class InstrumentServiceImpl implements InstrumentService {
                 .map(r -> ReagentHistoryUsage.builder()
                         .instrument(instrument)
                         .reagent(r)
+                        .quantityUsed(0.0)
                         .build())
                 .toList();
     }
