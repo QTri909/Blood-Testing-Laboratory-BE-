4c756446e0b365feda571f39f6a278b67ad4a7bd
brianHa <hhc9104@gmail.com>
2025-11-29 23:18:20 +0700

    feat: add kafka config for producer to patient-service

---
+++ patient-service/src/main/java/sum25/group03/patientservice/configs/KafkaConfig.java
@@ -0,0 +1,9 @@
+package sum25.group03.patientservice.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+}
